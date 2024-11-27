944aba69500aa515e08beb66c87e5b1afd95c2f1
Prozilla <business@prozilla.dev>
2024-11-27 09:25:44 +0100

    Added examples module

---
+++ examples/src/main/java/dev/prozilla/pine/examples/pacman/Main.java
@@ -0,0 +1,18 @@
+package dev.prozilla.pine.examples.pacman;
+
+import dev.prozilla.pine.core.Game;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		try {
+			Game game = new Game("PacMan", 1600, 900);
+			
+			game.init();
+			game.start();
+		} catch (RuntimeException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
