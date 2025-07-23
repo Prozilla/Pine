14a456ce8f8ac1348355c91193a9b02a4e84c0d0
Prozilla <business@prozilla.dev>
2025-07-23 15:14:35 +0200

    Fixed children and components of entities not being destroyed

--- pine-engine/src/main/java/dev/prozilla/pine/core/component/ComponentsProvider.java
+++ pine-engine/src/main/java/dev/prozilla/pine/core/component/ComponentsProvider.java
@@ -2,6 +2,7 @@ package dev.prozilla.pine.core.component;
 
 import dev.prozilla.pine.core.entity.Entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,26 +14,41 @@ public interface ComponentsProvider extends ComponentsContext {
 	
 	@Override
 	default <ComponentType extends Component> ComponentType getComponentInParent(Class<ComponentType> componentClass) {
+		if (getEntity() == null) {
+			return null;
+		}
 		return getEntity().getComponentInParent(componentClass);
 	}
 	
 	@Override
 	default <ComponentType extends Component> ComponentType getComponentInParent(Class<ComponentType> componentClass, boolean recursive) {
+		if (getEntity() == null) {
+			return null;
+		}
 		return getEntity().getComponentInParent(componentClass, recursive);
 	}
 	
 	@Override
 	default <ComponentType extends Component> List<ComponentType> getComponentsInChildren(Class<ComponentType> componentClass) {
+		if (getEntity() == null) {
+			return new ArrayList<>();
+		}
 		return getEntity().getComponentsInChildren(componentClass);
 	}
 	
 	@Override
 	default <ComponentType extends Component> ComponentType getComponent(Class<ComponentType> componentClass) {
+		if (getEntity() == null) {
+			return null;
+		}
 		return getEntity().getComponent(componentClass);
 	}
 	
 	@Override
 	default <ComponentType extends Component> List<ComponentType> getComponents(Class<ComponentType> componentClass) {
+		if (getEntity() == null) {
+			return new ArrayList<>();
+		}
 		return getEntity().getComponents(componentClass);
 	}
 	
--- pine-engine/src/main/java/dev/prozilla/pine/core/entity/EntityProvider.java
+++ pine-engine/src/main/java/dev/prozilla/pine/core/entity/EntityProvider.java
@@ -12,31 +12,49 @@ public interface EntityProvider extends EntityContext {
 	Entity getEntity();
 	
 	default Transform getTransform() {
+		if (getEntity() == null) {
+			return null;
+		}
 		return getEntity().transform;
 	}
 	
 	@Override
 	default Entity getFirstChild() {
+		if (getTransform() == null) {
+			return null;
+		}
 		return getTransform().getFirstChild();
 	}
 	
 	@Override
 	default Entity getLastChild() {
+		if (getTransform() == null) {
+			return null;
+		}
 		return getTransform().getLastChild();
 	}
 	
 	@Override
 	default boolean isDescendantOf(Transform parent) {
+		if (getTransform() == null) {
+			return false;
+		}
 		return getTransform().isDescendantOf(parent);
 	}
 	
 	@Override
 	default Entity getChildWithTag(String tag) {
+		if (getTransform() == null) {
+			return null;
+		}
 		return getTransform().getChildWithTag(tag);
 	}
 	
 	@Override
 	default Entity getParentWithTag(String tag) {
+		if (getTransform() == null) {
+			return null;
+		}
 		return getTransform().getParentWithTag(tag);
 	}
 }
