ab5b1c37f60cc852e54b3f265f4940920659d607
Prozilla <business@prozilla.dev>
2025-07-29 19:29:36 +0200

    Added application performance test

--- pine-engine/src/test/java/dev/prozilla/pine/core/ApplicationTest.java
+++ pine-engine/src/test/java/dev/prozilla/pine/core/ApplicationTest.java
@@ -1,19 +1,24 @@
 package dev.prozilla.pine.core;
 
+import dev.prozilla.pine.common.math.vector.Vector2f;
+import dev.prozilla.pine.core.entity.Entity;
+import dev.prozilla.pine.core.entity.prefab.shape.RectPrefab;
+import dev.prozilla.pine.core.entity.prefab.ui.*;
 import dev.prozilla.pine.core.scene.Scene;
-import dev.prozilla.pine.test.TestLoggingExtension;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import dev.prozilla.pine.core.scene.World;
+import dev.prozilla.pine.test.TestPerformanceExtension;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(TestLoggingExtension.class)
+@ExtendWith(TestPerformanceExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ApplicationTest {
+	
+	private static final int FRAMES = 60_000;
+	private static final float FPS = 60;
 	
 	@Test
 	void testLifecycle() {
@@ -26,6 +31,32 @@ public class ApplicationTest {
 		application.render(application.getRenderer());
 		application.destroy();
 		assertFalse(application.isRunning());
+	}
+	
+	@Test
+	@Tag("performance")
+	void testPerformance() {
+		Application application = mockApplication();
+		application.init();
+		
+		World world = application.getCurrentScene().getWorld();
+		
+		Entity nodeRoot = world.addEntity(new NodeRootPrefab());
+		nodeRoot.addChild(new LayoutPrefab());
+		nodeRoot.addChild(new TextPrefab("Test"));
+		nodeRoot.addChild(new TextInputPrefab());
+		nodeRoot.addChild(new TextButtonPrefab("Test"));
+		
+		world.addEntity(new RectPrefab(Vector2f.one()));
+		
+		for (int i = 0; i < FRAMES; i++) {
+			float deltaTime = 1f / FPS;
+			application.input(deltaTime);
+			application.update(deltaTime);
+			application.render(application.getRenderer());
+		}
+		
+		application.destroy();
 	}
 	
 	Application mockApplication() {
