31bdca58104e50e5fae967828e9ed79eace34637
Prozilla <business@prozilla.dev>
2025-10-28 14:49:52 +0100

    flappy-bird: Fixed gaps in background sprites

--- examples/flappy-bird/src/main/java/dev/prozilla/pine/examples/flappybird/entity/BackgroundPrefab.java
+++ examples/flappy-bird/src/main/java/dev/prozilla/pine/examples/flappybird/entity/BackgroundPrefab.java
@@ -1,8 +1,9 @@
 package dev.prozilla.pine.examples.flappybird.entity;
 
-import dev.prozilla.pine.core.scene.World;
+import dev.prozilla.pine.common.math.vector.Vector2f;
 import dev.prozilla.pine.core.entity.Entity;
 import dev.prozilla.pine.core.entity.prefab.sprite.SpritePrefab;
+import dev.prozilla.pine.core.scene.World;
 import dev.prozilla.pine.examples.flappybird.component.BackgroundData;
 
 public class BackgroundPrefab extends SpritePrefab {
@@ -12,6 +13,7 @@ public class BackgroundPrefab extends SpritePrefab {
 	public BackgroundPrefab() {
 		super("flappybird/background.png");
 		setName("Background");
+		setScale(new Vector2f(1.01f, 1.01f));
 		
 		index = 0;
 	}
