8942492b48a7c7b31c58a88c27b7e5f95d2c7cd0
Prozilla <business@prozilla.dev>
2025-11-06 13:46:14 +0100

    Updated ListUtils

--- pine-engine/src/main/java/dev/prozilla/pine/common/util/ListUtils.java
+++ pine-engine/src/main/java/dev/prozilla/pine/common/util/ListUtils.java
@@ -1,8 +1,16 @@
 package dev.prozilla.pine.common.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 
+/**
+ * Utility methods related to lists.
+ *
+ * <p>Some methods only work for lists, others work for all instances of a super class of {@link List}, like {@link Collection} or even {@link Iterable}.
+ * They are grouped together here for convenience.</p>
+ */
 public final class ListUtils {
 	
 	private ListUtils() {}
@@ -23,7 +31,7 @@ public final class ListUtils {
 	 * @param <E> The type of elements in the list
 	 * @param <T> The type of element to search for
 	 */
-	public static <E, T extends E> T getInstance(List<E> list, Class<T> type) {
+	public static <E, T extends E> T getInstance(Collection<E> list, Class<T> type) {
 		if (list.isEmpty()) {
 			return null;
 		}
@@ -34,6 +42,35 @@ public final class ListUtils {
 			}
 		}
 		
+		return null;
+	}
+	
+	/**
+	 * Returns the first element in a list, or {@code null} if the list is empty.
+	 * @param list The list
+	 * @return The first element or {@code null}.
+	 * @param <E> The type of elements in the list
+	 */
+	public static <E> E getFirst(List<E> list) {
+		if (list.isEmpty()) {
+			return null;
+		}
+		return list.getFirst();
+	}
+	
+	/**
+	 * Finds the first element in a list that matches a predicate.
+	 * @param list The list
+	 * @param predicate The predicate the element must match
+	 * @return The first element that matches the predicate, or {@code null} if there was none.
+	 * @param <E> The type of elements in the list
+	 */
+	public static <E> E find(Iterable<E> list, Predicate<E> predicate) {
+		for (E element : list) {
+			if (predicate.test(element)) {
+				return element;
+			}
+		}
 		return null;
 	}
 	
